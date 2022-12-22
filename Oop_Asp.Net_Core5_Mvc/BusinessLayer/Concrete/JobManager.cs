e8d450af0d9c6d93caafb3798440a4c12316d766
gokcekadr@gmail.com <gokcekadr@gmail.com>
2022-12-22 16:01:20 +0300

    Add project files.

---
+++ Oop_Asp.Net_Core5_Mvc/BusinessLayer/Concrete/JobManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class JobManager : IGenericService <Job>
+    {
+        IJobDal _jobDal;
+
+        public JobManager(IJobDal jobDal)
+        {
+            _jobDal = jobDal;
+        }
+
+        public List<Job> GetList()
+        {
+            return _jobDal.GetList();
+        }
+
+        public void TDelete(Job t)
+        {
+            _jobDal.Delete(t);
+        }
+
+        public Job TGetById(int id)
+        {
+            return _jobDal.GetById(id);
+        }
+
+        public void TInsert(Job t)
+        {
+            _jobDal.Insert(t);
+        }
+
+        public void TUpdate(Job t)
+        {
+            _jobDal.Update(t);
+        }
+    }
+}
---
+++ Oop_Asp.Net_Core5_Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Oop_Asp.Net_Core5_Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
